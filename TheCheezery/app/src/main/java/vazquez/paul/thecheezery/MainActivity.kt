969cd87acf828d5864efa8941d00465a2208f0c2
polpoliester <pol_poliester@hotmail.com>
2025-06-12 15:38:16 -0700

    Avance
    activity_main

---
+++ TheCheezery/app/src/main/java/vazquez/paul/thecheezery/MainActivity.kt
@@ -0,0 +1,21 @@
+package vazquez.paul.thecheezery
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import vazquez.paul.thecheezery.R.id
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
