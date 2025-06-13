718e9e5a376dd5d5f2baa7873ee63fe7f355dd3e
polpoliester <pol_poliester@hotmail.com>
2025-06-13 00:15:08 -0700

    Get Started, Login

--- TheCheezery/app/src/main/java/vazquez/paul/thecheezery/MainActivity.kt
+++ TheCheezery/app/src/main/java/vazquez/paul/thecheezery/MainActivity.kt
@@ -1,21 +1,20 @@
 package vazquez.paul.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import vazquez.paul.thecheezery.R.id
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button: Button = findViewById<Button>(R.id.button_get_started) as Button
+        button.setOnClickListener {
+            //Moverse a pantalla Register
+            var intent: Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
